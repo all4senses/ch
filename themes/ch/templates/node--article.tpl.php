253d0baed5c2945fb87e8dd161092c9648f17b85
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-07-11 19:35:46 +0300

    Move site to the main domain

--- themes/ch/templates/node--article.tpl.php
+++ themes/ch/templates/node--article.tpl.php
@@ -9,8 +9,6 @@
     $extra_data['guest_author'] = $author_name = !empty($extra_data['guest_author']) ? $extra_data['guest_author'] : NULL;
   }
 
-  //dpm($extra_data);
-  
   if (!$extra_data['guest_author']) {
     $authorExtendedData = ch_misc_loadUserExtendedData($node->uid);
     $author_name = $authorExtendedData->realname;
