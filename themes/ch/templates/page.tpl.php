537da78ab4a2253ba0b4b902fa44e9458269afb5
U-FLYSMALL\Alex <all4senses@gmail.com>
2013-08-26 22:16:05 +0700

    Home restyling

--- themes/ch/templates/page.tpl.php
+++ themes/ch/templates/page.tpl.php
@@ -65,9 +65,9 @@
       
       <?php if ($page['highlighted']): ?>
           <?php 
-            $url = 'http://cloudhostinghq.com';
-            $share_title = ch_misc_metatag_getFrontTitle();
-            echo '<div class="float share">' . ch_blocks_getSocialiteButtons($url, $share_title) . '</div>';
+//            $url = 'http://cloudhostinghq.com';
+//            $share_title = ch_misc_metatag_getFrontTitle();
+//            echo '<div class="float share">' . ch_blocks_getSocialiteButtons($url, $share_title) . '</div>';
           ?>
           <section id="highlighted" class="clearfix">
             <?php print render($page['highlighted']); ?>
